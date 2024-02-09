45370af5720c5b4587c58addf6f656afb16ae211
Dani <daniceuve@gmail.com>
2024-02-09 08:18:27 +0100

    Modificaciones

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/Main.java
@@ -0,0 +1,11 @@
+package org.iesalandalus.programacion.tallermecanico;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Cliente.java
@@ -0,0 +1,16 @@
+package org.iesalandalus.programacion.tallermecanico.modelo.dominio;
+
+public class Cliente {
+
+    private static final String ER_NOMBRE = "a";
+    private static final String ER_DNI = "e";
+    private static final String ER_TELEFONO = "o";
+    private String nombre;
+    private String dni;
+    private String telefono;
+
+
+
+
+
+}
