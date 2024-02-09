af885a85eff676c87b69438e91e816ecf072bd88
Dani <daniceuve@gmail.com>
2024-02-09 08:28:53 +0100

    Modificaciones

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Cliente.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Cliente.java
@@ -2,9 +2,9 @@ package org.iesalandalus.programacion.tallermecanico.modelo.dominio;
 
 public class Cliente {
 
-    private static final String ER_NOMBRE = "a";
-    private static final String ER_DNI = "e";
-    private static final String ER_TELEFONO = "o";
+    private static final String ER_NOMBRE = "([A-Z][a-z]+)(\\s[A-Z][a-z]+)?";
+    private static final String ER_DNI = "([0-9]{8})([A-Za-z])";
+    private static final String ER_TELEFONO = "[96]?[0-9]{8}";
     private String nombre;
     private String dni;
     private String telefono;
