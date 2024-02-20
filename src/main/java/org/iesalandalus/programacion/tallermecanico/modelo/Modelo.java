207b25f2c714660387a33f5582ee1e92c341cf1e
Dani <daniceuve@gmail.com>
2024-02-20 10:20:13 +0100

    Modificaciones

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/Modelo.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/Modelo.java
@@ -12,7 +12,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 
 public class Modelo {
 
@@ -20,8 +19,7 @@ public class Modelo {
     Vehiculos vehiculos;
     Revisiones revisiones;
 
-    public Modelo(){
-
+    public Modelo() {
 
     }
 
@@ -30,33 +28,35 @@ public class Modelo {
         vehiculos = new Vehiculos();
         revisiones = new Revisiones();
     }
+
     public void terminar() {
         System.out.println("Terminado");
     }
-    public void insertar(Cliente cliente) {
 
-    }
-    public void insertar(Vehiculo vehiculo) {
-
-    }
-    public void insertar(Revision revision) {
-
+    public void insertar(Cliente cliente) throws OperationNotSupportedException {
+        clientes.insertar(new Cliente(cliente));
     }
 
-    public Cliente buscar(Cliente cliente) throws OperationNotSupportedException{
-        if (clientes.buscar(cliente) == null)
-            throw new OperationNotSupportedException("buscar cliente");
-        return new Cliente(cliente);
+    public void insertar(Vehiculo vehiculo) throws OperationNotSupportedException {
+        vehiculos.insertar(vehiculo);
     }
-    public Vehiculo buscar(Vehiculo vehiculo) throws OperationNotSupportedException{
-        if (vehiculos.buscar(vehiculo) == null)
-            throw new OperationNotSupportedException("buscar vehiculo");
-        return new Vehiculo(vehiculo.marca(), vehiculo.modelo(), vehiculo.matricula());
+
+    public void insertar(Revision revision) throws OperationNotSupportedException {
+        revisiones.insertar(new Revision(buscar(revision.getCliente()), buscar(revision.getVehiculo()), revision.getFechaInicio()));
     }
-    public Revision buscar(Revision revision) throws OperationNotSupportedException{
-        if (revisiones.buscar(revision) == null)
-            throw new OperationNotSupportedException("buscar revision");
+
+    public Cliente buscar(Cliente cliente) {
+        return clientes.buscar(cliente);
     }
+
+    public Vehiculo buscar(Vehiculo vehiculo) {
+        return vehiculos.buscar(vehiculo);
+    }
+
+    public Revision buscar(Revision revision) {
+        return revisiones.buscar(revision);
+    }
+
     public boolean modificar(Cliente cliente, String nombre, String telefono) throws OperationNotSupportedException {
         return clientes.modificar(cliente, nombre, telefono);
     }
@@ -64,12 +64,15 @@ public class Modelo {
     public void anadirHoras(Revision revision, int horas) throws OperationNotSupportedException {
         revisiones.anadirHoras(revision, horas);
     }
+
     public void anadirPrecioMaterial(Revision revision, float precioMaterial) throws OperationNotSupportedException {
         revisiones.anadirPrecioMaterial(revision, precioMaterial);
     }
+
     public void cerrar(Revision revision, LocalDate fechaFin) throws OperationNotSupportedException {
         revisiones.cerrar(revision, fechaFin);
     }
+
     public void borrar(Cliente cliente) throws OperationNotSupportedException {
         Objects.requireNonNull(cliente, "El cliente no puede ser nulo.");
         List<Revision> listaRevisionesClienteBorrar = new ArrayList<>(revisiones.get(cliente));
@@ -78,6 +81,7 @@ public class Modelo {
         }
         clientes.borrar(cliente);
     }
+
     public void borrar(Vehiculo vehiculo) throws OperationNotSupportedException {
         Objects.requireNonNull(vehiculo, "El vehículo no puede ser nulo.");
         List<Revision> listaRevisionesVehiculoBorrar = new ArrayList<>(revisiones.get(vehiculo));
@@ -86,24 +90,47 @@ public class Modelo {
         }
         vehiculos.borrar(vehiculo);
     }
+
     public void borrar(Revision revision) throws OperationNotSupportedException {
         Objects.requireNonNull(revision, "La revisión no puede ser nula.");
         revisiones.borrar(revision);
     }
+
     public List<Cliente> getClientes() {
 
-    }
-    public List<Vehiculo> getVehiculo() {
+        List<Cliente> listaClientes = new ArrayList<>();
+        for (Cliente recorrer : clientes.get())
+            listaClientes.add(new Cliente(recorrer));
 
+        return listaClientes;
     }
-    public List<Revision> getRevision() {
 
+    public List<Vehiculo> getVehiculos() {
+        return new ArrayList<>(vehiculos.get());
     }
+
+    public List<Revision> getRevisiones() {
+        List<Revision> listaRevisiones = new ArrayList<>();
+        for (Revision recorrer : revisiones.get())
+            listaRevisiones.add(new Revision(recorrer));
+        return listaRevisiones;
+    }
+
     public List<Revision> getRevisiones(Cliente cliente) {
 
+        List<Revision> listaRevisionesCliente = new ArrayList<>();
+        for (Revision recorrer : revisiones.get(cliente))
+            if (recorrer.getCliente().equals(cliente))
+                listaRevisionesCliente.add(recorrer);
+        return listaRevisionesCliente;
     }
-    public List<Revision> getRevisiones(Vehiculo vehiculo) {
 
+    public List<Revision> getRevisiones(Vehiculo vehiculo) {
+        List<Revision> listaRevisionesVehiculo = new ArrayList<>();
+        for (Revision recorrer : revisiones.get(vehiculo))
+            if (recorrer.getVehiculo().equals(vehiculo))
+                listaRevisionesVehiculo.add(recorrer);
+        return listaRevisionesVehiculo;
     }
 
 }
