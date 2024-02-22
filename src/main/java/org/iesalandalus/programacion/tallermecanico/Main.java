02dd35b99e4c8174ed5b6cf218fcb11349a0c036
Dani <daniceuve@gmail.com>
2024-02-22 14:35:32 +0100

    Fin Bien

--- src/main/java/org/iesalandalus/programacion/tallermecanico/Main.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/Main.java
@@ -5,14 +5,10 @@ import org.iesalandalus.programacion.tallermecanico.modelo.Modelo;
 import org.iesalandalus.programacion.tallermecanico.vista.Vista;
 
 public class Main {
-
     public static void main(String[] args) {
-
         Modelo modelo = new Modelo();
         Vista vista = new Vista();
         Controlador controlador = new Controlador(modelo, vista);
         controlador.comenzar();
-
     }
-
 }
